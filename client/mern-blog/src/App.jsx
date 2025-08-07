181d0142b499053f7712eadadbddce6547eb0624
janet-mutisya <janetymutisya@gmail.com>
2025-08-07 21:31:59 +0300

    final

--- client/mern-blog/src/App.jsx
+++ client/mern-blog/src/App.jsx
@@ -1,7 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import  Signup  from "./pages/Signup";
+import Signup from "./pages/Signup";
 import Activity from "./pages/Activity";
 import BugList from "@/pages/BugList";
 import BugForm from "./pages/BugForm";
@@ -12,14 +12,17 @@ export default function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="activity" element={<Activity/>} />
+        {/*Redirect from root path to /login */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/buglist" element={<BugList />} />
         <Route path="/bugForm" element={<BugForm />} />
-       
+        <Route path="/activity" element={<Activity />} />
       </Routes>
     </Router>
   );
 }
+
